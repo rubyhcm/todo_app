9b87e1f799b7020f8f6195b462ff78061e8cdbfa
itwebhcm <itweb.hcm@gmail.com>
2024-09-15 10:28:50 -0700

    class_1: route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,18 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  # get "/articles" => "articles#list"
+  # get "/articles", to: "articles#list"
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  # root to: "home#welcome"
+
+  # get 'photos/:id', to: 'photos#show', defaults: { format: 'jpg' }
+  # Rails would match photos/12 to the show action of PhotosController, and set params[:format] to "jpg".
+
+  # get 'exit', to: 'sessions#destroy', as: :logout
+
+  # get 'photos/*other', to: 'photos#unknown'
+  # This route would match photos/12 or /photos/long/path/to/12, setting params[:other] to "12" or "long/path/to/12".
+
+  # get 'books/*section/:title', to: 'books#show'
+  # would match books/some/section/last-words-a-memoir with params[:section] equals 'some/section', and params[:title] equals 'last-words-a-memoir'.
+
 end
