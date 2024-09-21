20c8a8956bbc7d72f244e78870401c31c080fa3c
itwebhcm <itweb.hcm@gmail.com>
2024-09-20 20:39:35 -0700

    class_3: add validate

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_09_20_163125) do
+ActiveRecord::Schema[7.0].define(version: 2024_09_21_031442) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "tasks", force: :cascade do |t|
-    t.string "title"
+    t.string "title", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
