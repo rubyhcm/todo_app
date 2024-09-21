b8455bfcd4ab685672c908c43b0aa94c76aa7703
itwebhcm <itweb.hcm@gmail.com>
2024-09-21 06:17:30 -0700

    class_6: Set up react environment

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,1 +1,12 @@
-import "../stylesheets/application.scss"
+// This file is automatically compiled by Webpack, along with any other files
+// present in this directory. You're encouraged to place your actual application logic in
+// a relevant structure within app/javascript and only use these pack files to reference
+// that code so it'll be compiled.
+
+import "../stylesheets/application.scss";
+
+const { setAuthHeaders } = require("apis/axios");
+const { initializeLogger } = require("common/logger");
+
+initializeLogger();
+setAuthHeaders();
